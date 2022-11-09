93249db45fc33be439f89d0e01c5d4abc7007bd6
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-08 17:33:24 -0700

    added a few global variables to js

---
+++ Main/Assets/script.js
@@ -0,0 +1,9 @@
+//global variables
+const weatherApiRootUrl = 'https://api.openweathermap.org';
+const weatherApiKey = 'ac50d784bf914987a46bfec9b1157873';
+let history = [];
+//DOM element variables
+
+//timezones
+
+//functions to display results, history, and to update history
