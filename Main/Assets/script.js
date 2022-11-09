8d15a153c4b73795511414286428bfd176655f94
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-08 17:53:37 -0700

    added DOM variables to js, and added dayjs plugins as well

--- Main/Assets/script.js
+++ Main/Assets/script.js
@@ -3,7 +3,12 @@ const weatherApiRootUrl = 'https://api.openweathermap.org';
 const weatherApiKey = 'ac50d784bf914987a46bfec9b1157873';
 let history = [];
 //DOM element variables
-
-//timezones
-
+let searchForm = document.querySelector('#search-form');
+let searchInput = document.querySelector('#search-input');
+let today = document.querySelector('#today');
+let forecast = document.querySelector('#forecast');
+let searchHistory = document.querySelector('#history');
+//dayjs
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 //functions to display results, history, and to update history
